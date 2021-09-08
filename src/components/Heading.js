37d2bbd09375bf376bb4ca366f2b81be85cca481
Maria Victoria <marivibilbaog@gmail.com>
2021-09-09 01:13:01 +0200

    Agrego imagen de logo

--- src/components/Heading.js
+++ src/components/Heading.js
@@ -1,7 +1,17 @@
+//Lesson 1:
 import React from "react";
 
 const Heading = () => {
-  return <header className="App-header">CYF Hotel</header>;
+  return (
+    <header className="App-header">
+      CYF Hotel
+      <img
+        class="App-logo"
+        src="https://image.flaticon.com/icons/svg/139/139899.svg"
+        alt="The hotel's logo"
+      />
+    </header>
+  );
 };
 
 export default Heading;
