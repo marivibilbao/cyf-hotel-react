83bb1153289ede4223bbfbc6f4f960221d186210
Maria Victoria <marivibilbaog@gmail.com>
2021-09-15 23:16:18 +0200

    Agrego archivos js

---
+++ src/components/RestaurantOrders.js
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+
+const Order = ({ orderType }) => {
+  const [order, setOrder] = useState(0);
+  const orderOne = () => setOrder(order + 1);
+
+  return (
+    <li className="m-3 d-flex justify-content-center">
+      {orderType} : {order} <RestaurantButton orderOne={orderOne} />
+    </li>
+  );
+};
+
+export default Order;
