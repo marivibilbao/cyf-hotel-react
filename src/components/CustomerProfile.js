9ba79efc7060a87548cacc8e0e8566900b91befc
Maria Victoria <marivibilbaog@gmail.com>
2021-09-23 15:40:41 +0200

    Agrego modificaciones

--- src/components/CustomerProfile.js
+++ src/components/CustomerProfile.js
@@ -1,9 +1,5 @@
 import React from "react";
 
 export default function CustomerProfile(props) {
-  return (
-    <div>
-      <p>Customer Profile</p>
-    </div>
-  );
+  return <div>{/* <p>Customer Profile</p> */}</div>;
 }
